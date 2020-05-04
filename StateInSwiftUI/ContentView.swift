79dd050c77ed291e2d653561672612e721262039
fatehkhann <fatehkhann@gmail.com>
2020-05-04 21:24:41 +0500

    Initial Commit

---
+++ StateInSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  StateInSwiftUI
+//
+//  Created by Fateh Khan on 04/05/2020.
+//  Copyright © 2020 Zarrar Company. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
